014f85b2e99fcf3072970ba8f6a83930eb45132a
João Moreira <110946307+Joao-H-Moreira@users.noreply.github.com>
2024-06-17 22:25:42 -0300

    Update pacientesmain.js

--- pacientesmain.js
+++ pacientesmain.js
@@ -1,24 +1,3 @@
-// const formCadastroPaciente = document.getElementById('form-cadastro-paciente');
-// const formAgendamentoConsulta = document.getElementById('form-agendamento-consulta');
-// formAgendamentoConsulta.addEventListener('submit', (event) => {
-//   event.preventDefault();
-
-//   const paciente_id = document.getElementById('paciente').value;
-//   const profissional_id = document.getElementById('profissional').value;
-//   const data = document.getElementById('data').value;
-//   const horario = document.getElementById('horario').value;
-
-//   // Validar agendamento
-
-//   // Agendar consulta no banco de dados
-
-//   // Limpar o formulário
-//   document.getElementById('paciente').value = '';
-//   document.getElementById('profissional').value = '';
-//   document.getElementById('data').value = '';
-//   document.getElementById('horario').value = '';
-// });
-
 var pacientesVue = new Vue({
   el: "#pacientesVue",
   data: () => {
