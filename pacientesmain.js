93a400d2c492d314f93932e863312c62fe1e6aeb
João Moreira <110946307+Joao-H-Moreira@users.noreply.github.com>
2024-06-21 03:30:01 -0300

    Add files via upload

--- pacientesmain.js
+++ pacientesmain.js
@@ -1,30 +1,54 @@
 var pacientesVue = new Vue({
   el: "#pacientesVue",
   data: () => {
-      return {
-        pacienteObj:{
-           nome:"", dataNascimento:"", telefone:"", endereco:""
-        }
-        ,agendamentoObj:{
-          data:"", horario:"", profissional:"", paciente:""
-        }  
-        ,listaPacientes:[]
-        ,listaAgendamento:[]
-      }
+    return {
+      pacienteObj: {
+        nome: "",
+        dataNascimento: "",
+        telefone: "",
+        endereco: "",
+      },
+      agendamentoObj: {
+        data: "",
+        horario: "",
+        profissional: "",
+        paciente: "",
+        compareceu:false,
+        faltou:false,
+        id:0,
+      },
+      listaPacientes: [],
+      listaAgendamento: [],
+      listaMedicos:[],
+    };
   },
   mounted() {
-    if(localStorage.getItem("listaPacientes")!=null ){ 
-      this.listaPacientes=JSON.parse(localStorage.getItem("listaPacientes"))}
-   
+    if (localStorage.getItem("listaPacientes") != null) {
+      this.listaPacientes = JSON.parse(localStorage.getItem("listaPacientes"));
+    }
+    if (localStorage.getItem("listaMedicos") != null) {
+      this.listaMedicos = JSON.parse(localStorage.getItem("listaMedicos"));
+    }
+    if (localStorage.getItem("listaAgendamento") != null) {
+      this.listaAgendamento = JSON.parse(localStorage.getItem("listaAgendamento"));
+    }
   },
+
   methods: {
-      cadastrar(){
-       this.listaPacientes.push(this.pacienteObj)
-       localStorage.setItem("listaPacientes",JSON.stringify (this.listaPacientes))
-      }
-      ,agendar(){
-       this.listaAgendamento.push(this.agendamentoObj)
-      }
-      }
-  }
-)
+    cadastrar() {
+      this.listaPacientes.push(this.pacienteObj);
+      localStorage.setItem(
+        "listaPacientes",
+        JSON.stringify(this.listaPacientes)
+      );
+    },
+    agendar() {
+      this.agendamentoObj.id= Date.now()
+      this.listaAgendamento.push(this.agendamentoObj);
+      localStorage.setItem(
+        "listaAgendamento",
+        JSON.stringify(this.listaAgendamento)
+      );
+    },
+  },
+});
--- profissionaismain.js
+++ profissionaismain.js
@@ -1,23 +1,53 @@
-
-
 var profissionaisVue = new Vue({
   el: "#profissionaisVue",
   data: () => {
-      return {
-          atendimentoPacienteObj:{
-            consulta, avaliacao,
-          }
-          ,atendimentoPaciente:[]
-      }
+    return {
+      atendimentoPacienteObj: {
+        consulta: "",
+        avaliacao: "",
+      },
+      selecionado:{} ,
+      indice:0,
+      faltou: false,
+      compareceu: false,
+      atendimentoPaciente: [],
+      listaPacientes: [],
+      listaAgendamento: [],
+    };
   },
   mounted() {
-    if(localStorage.getItem("atendimentoPaciente")!=null ){ 
-      this.atendimentoPaciente=JSON.parse(localStorage.getItem("atendimentoPaciente"))}
+    if (localStorage.getItem("atendimentoPaciente") != null) {
+      this.atendimentoPaciente = JSON.parse(
+        localStorage.getItem("atendimentoPaciente")
+      );
+    }
+
+    if (localStorage.getItem("listaPacientes") != null) {
+      this.listaPacientes = JSON.parse(localStorage.getItem("listaPacientes"));
+    }
+    if (localStorage.getItem("listaAgendamento") != null) {
+      this.listaAgendamento = JSON.parse(
+        localStorage.getItem("listaAgendamento")
+      );
+    }
   },
   methods: {
-    registrar(){
-      this.atendimentoPaciente.push(this.atendimentoPacienteObj)
-      localStorage.setItem("atendimentoPaciente",JSON.stringify (this.atendimentoPaciente))
-     }
-  }
-})
+    registrar() {
+      this.atendimentoPaciente.push(this.atendimentoPacienteObj);
+      localStorage.setItem(
+        "atendimentoPaciente",
+        JSON.stringify(this.atendimentoPaciente)
+      );
+    },
+    confirmar() {
+      for (let i = 0; i < this.listaAgendamento.length; i++) {  
+        if (this.listaAgendamento[i].id.toString()==this.selecionado.split(", ")[3]) {
+          this.listaAgendamento[i].compareceu=this.compareceu;
+          this.listaAgendamento[i].faltou=this.faltou;
+        }
+        
+      }
+      localStorage.setItem("listaAgendamento",JSON.stringify(this.listaAgendamento));
+    },
+  },
+});
